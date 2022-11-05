ea6471d848bf77c07fdd127b4845bc676870c06e
IMayanLP <mayan20119@gmail.com>
2022-11-05 18:31:04 -0300

    Ta ai likotrico, pode copiar

--- main.py
+++ main.py
@@ -1,47 +1,28 @@
 import pandas as pd
-import sklearn.model_selection as ms
-import sklearn.linear_model as lm
 import matplotlib.pyplot as plt
-import numpy as np
 
+from sklearn.model_selection import cross_val_score # função de validação cruzada
+from sklearn.model_selection import KFold # KFold, só isso mesmo.
+from sklearn.linear_model import LinearRegression # função de regressão linear
 
 # Importando o banco e dividindo as colunas
-dataset = pd.read_csv("db_test.csv") # Carrega o banco
-dataset = dataset.dropna() # Exclui os nulos
-X = dataset.iloc[:, :-1] # Pega todas as colunas menos a ultima
-y = dataset.iloc[:, -1] # Pega apenas a ultima coluna
+db = pd.read_csv("db_test.csv") # Carrega o banco
+db = db.dropna() # Exclui os nulos
+X = db.iloc[:, :-1] # Pega todas as colunas menos a ultima
+y = db.iloc[:, -1] # Pega apenas a ultima coluna
 
 # Codificando variaveis Dummy
 X_dummies = pd.get_dummies(X) # Classificando variaveis não quantificativas (Dummies)
+# inclui variáveis categóricas ao modelo (que só aceita variáveis numéricas)
 
-# Separando dados em treino e teste
-X_train, X_test, y_train, y_test = ms.train_test_split(X_dummies, y, test_size=1/7, random_state=0)
+modelo  = LinearRegression() # criando modelo de regressão linear
+# configurando o K-Fold com K = 10 e embaralhamento ligado
+kfold  = KFold(n_splits=10, shuffle=True)
+# chama a função de validação cruzada com nosso modelo, X, y
+# e determina a estratégia de divisão de validação cruzada (no caso kfold declarado acima)
+result = cross_val_score(modelo, X_dummies, y, cv = kfold)
 
-# Treinando o modelo
-regressor = lm.LinearRegression()
-regressor.fit(X_train, y_train)
+print("pontuações R² de cada K-Fold: {0}".format(result))
+print("Média dos R²: {0}".format(result.mean()))
 
-# Criando conjunto de predict a partir do modelo treinado
-y_pred = regressor.predict(X_test) # Prevendo valores de X_test
-
-np.set_printoptions(precision=2) # Configurando precisão dos numeros
-# Concatenando o conjunto de predict com o conjunto de teste em colunas
-result = np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.values.reshape(-1, 1)), 1)
-
-# Re-ordenando o conjunto de teste
-X_test = X_test.reset_index(drop=True)
-
-# Criando conjuntos com os dados de result
-y_compare = pd.DataFrame(result)
-# Renomeando colunas
-y_compare = y_compare.rename(index=str, columns={0:'y_pred', 1:'y_test'})
-# Re-ordenando o conjunto y_compare
-y_compare = y_compare.reset_index(drop=True)
-
-# Concatenando as colunas da comparação e testes
-result_final = pd.concat([y_compare, X_test], axis=1)
-
-# Criando R²
-from sklearn.metrics import r2_score
-r2 = r2_score(y_test, y_pred)
 
